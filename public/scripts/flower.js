12d192d02f0f7c12e0fb834dbae135b435b002b3
SY <oowwo@naver.com>
2022-02-21 23:47:46 +0900

    Add:버튼 클릭시 슬라이드 구현

---
+++ public/scripts/flower.js
@@ -0,0 +1,71 @@
+const btnNext = document.querySelector(".next");
+const btnPrev = document.querySelector(".prev");
+const flowerProduct = document.querySelector(".flower-products");
+
+const image1 = document.getElementById("image-1");
+const image2 = document.getElementById("image-2");
+const image3 = document.getElementById("image-3");
+
+const text1 = document.getElementById("text-1");
+const text2 = document.getElementById("text-2");
+const text3 = document.getElementById("text-3");
+
+const price1 = document.getElementById("price-1");
+const price2 = document.getElementById("price-2");
+const price3 = document.getElementById("price-3");
+
+const slideData = [
+  {
+    img: "/public/assets/flower_event_1.jpeg",
+    title: "ㅎㅎ",
+    price: "79,000원",
+    description: "1월 플라워 클래스",
+  },
+  {
+    img: "/public/assets/flower_event_2.jpg",
+    title: "오아시스 리스(2/21~2/27)",
+    price: "79,000원",
+    description: "1월 플라워 클래스",
+  },
+  {
+    img: "/public/assets/flower_event_3.jpg",
+    title: "클래식 핸드타이드(2/28~3/6)",
+    price: "79,000원",
+    description: "1월 플라워 클래스",
+  },
+];
+
+//Next 버튼을 누르면
+//data 배열에 있는 객체값들을
+//바꿔서 dom요소에 넣어준다?
+
+var index = 0;
+
+btnNext.addEventListener("click", (event) => {
+  index++;
+  var data = slideData.map((_, i, data) => data[(i + index) % 3]);
+  renderItem(data);
+});
+
+btnPrev.addEventListener("click", (event) => {
+  index--;
+  if (index < 0) {
+    index = 2;
+  }
+  var data = slideData.map((_, i, data) => data[(i + index) % 3]);
+  renderItem(data);
+});
+
+function renderItem(data) {
+  image1.src = data[0].img;
+  image2.src = data[1].img;
+  image3.src = data[2].img;
+
+  text1.innerText = data[0].description;
+  text2.innerText = data[1].title;
+  text3.innerText = data[2].title;
+
+  price1.innerText = data[0].description;
+  price2.innerText = data[1].price;
+  price3.innerText = data[2].price;
+}
